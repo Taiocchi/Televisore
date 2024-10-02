bd13b0c5d9208b657eb7fe94ba7a2040f96ff879
Luca <152988683+Taiocchi@users.noreply.github.com>
2024-10-02 13:34:57 +0200

    Add files via upload

---
+++ Televisore/Implementazione/Televisione/Program.cs
@@ -0,0 +1,32 @@
+﻿class main
+{
+    static void Main()
+    {
+        
+    }
+}
+
+class Televisore
+{
+    int consumo;
+    bool stato;
+    int stato_canale;
+    int stato_volume;
+
+    void accendi()
+    {
+        stato = true;
+    }
+    void spegni()
+    {
+        stato = false;
+    }
+    void cambia_canale(int canale_nuovo)
+    {
+        stato_canale = canale_nuovo;
+    }
+    void cambia_volume(int volume_nuovo)
+    {
+        stato_volume = volume_nuovo;
+    }
+}
